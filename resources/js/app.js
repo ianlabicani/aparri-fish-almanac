bf53a6f018eb3eff8f614c0f92e5595e38bb4a5a
ianlabicani <labicaniian@gmail.com>
2025-02-14 00:21:20 +0800

    fix bootstrap import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,8 @@
 import "./bootstrap";
 
 import Alpine from "alpinejs";
-import bootstrap from "bootstrap";
+import "bootstrap";
 
-window.bootstrap = bootstrap;
 window.Alpine = Alpine;
 
 Alpine.start();
