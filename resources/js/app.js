1fce5cf594c138badf4b3668a2ed3aba9a127a90
ianlabicani <labicaniian@gmail.com>
2025-02-13 23:03:38 +0800

    add bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
+import bootstrap from "bootstrap";
 
+window.bootstrap = bootstrap;
 window.Alpine = Alpine;
 
 Alpine.start();
